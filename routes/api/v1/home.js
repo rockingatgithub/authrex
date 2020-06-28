2d6faec0091d9809e1d0823d082cfd7893faba9e
rockingatgithub <contactwithsid@gmail.com>
2020-06-28 13:09:29 +0530

    error catched

--- routes/api/v1/home.js
+++ routes/api/v1/home.js
@@ -3,8 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const homeApi = require("../../../controllers/api/v1/home_api");
-const { home } = require("../../../controllers/home_controller");
 
-router.get("/", homeApi.index);
+router.get("/", homeApi.createSession);
 
 module.exports = router;
