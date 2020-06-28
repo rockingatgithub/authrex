34b277d043e75bca550702a054a67b978f59a953
rockingatgithub <contactwithsid@gmail.com>
2020-06-28 13:06:32 +0530

    jwt setup

--- controllers/api/v1/home_api.js
+++ controllers/api/v1/home_api.js
@@ -1,5 +1,31 @@
-module.exports.index = function (req, res) {
-  return res.json(200, {
-    message: "Successfully Login",
-  });
+// module.exports.index = function (req, res) {
+//   return res.json(200, {
+//     message: "Successfully Login",
+//   });
+// };
+
+const User = require("../../../models/user");
+const jwt = require("jsonwebtoken");
+
+module.exports.createSession = async function (req, res) {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+
+    if (!user || user.password != req.body.password) {
+      return res.json(422, {
+        message: "Invalid Username/password",
+      });
+    }
+    return res.json(200, {
+      message: "Sign in successful, here is your token...",
+      data: {
+        token: jwt.sign(user.toJSON(), "authrex", { expiresIn: "10000" }),
+      },
+    });
+  } catch (err) {
+    console.log("*******", err);
+    return res.json(500, {
+      message: "Internal Server Error",
+    });
+  }
 };
